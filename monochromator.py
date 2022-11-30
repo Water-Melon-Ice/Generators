41843b743d7d059d7beb9188bb2f6b1c09e883d4
Adrian Weber <47919471+Water-Melon-Ice@users.noreply.github.com>
2022-11-30 01:06:00 +0100

    Add rgb invert

--- monochromator.py
+++ monochromator.py
@@ -21,19 +21,22 @@ def pixels_from_image(image):
 def do_stuff():
     if(len(sys.argv) > 1):
         if(sys.argv[1] == "help"):
-            print("Usage: monochromator.py <options: help, pt: <file> [channel, default:0], sc: <file> <red%> <green%> <blue%>")
+            print("Usage: monochromator.py <options: help, sc: <file> [channel, default:0] [invert], pt:  <file> <red%> <green%> <blue%> [invert]>")
             return
         elif(sys.argv[1] == "pt"):
             if(len(sys.argv) > 2):
                 red = 0.33
                 green = 0.33
                 blue = 0.33
+                invert = False
                 if(len(sys.argv) > 3):
                     red = int(sys.argv[3]) / 100.0
                 if(len(sys.argv) > 4):
                     green = int(sys.argv[4]) / 100.0
                 if(len(sys.argv) > 5):
                     blue = int(sys.argv[5]) / 100.0
+                if(len(sys.argv) > 6):
+                    invert = True
                 file = sys.argv[2]
                 img = open_image(file)
                 pixels = pixels_from_image(img)
@@ -42,12 +45,19 @@ def do_stuff():
                     for x in range(width):
                         color = int(pixels[x,y][0] * red + pixels[x,y][1] * green + pixels[x,y][2] * blue)
                         pixels[x,y] = (color, color, color)
-                save_image(img, str(red) + "r_" + str(green) + "g_" + str(blue) + "b_" + file)
+                if(invert):
+                    for y in range(height):
+                        for x in range(width):
+                            pixels[x,y] = (255 - pixels[x,y][0], 255 - pixels[x,y][1], 255 - pixels[x,y][2])
+                save_image(img, str(invert) + "-inverted_" + str(red) + "r_" + str(green) + "g_" + str(blue) + "b_" + file)
         elif(sys.argv[1] == "sc"):
             if(len(sys.argv) > 2):
                 channel = 0
+                invert = False
                 if(len(sys.argv) > 3):
                     channel = int(sys.argv[3])
+                if(len(sys.argv) > 4):
+                    invert = True
                 file = sys.argv[2]
                 img = open_image(file)
                 pixels = pixels_from_image(img)
@@ -55,7 +65,11 @@ def do_stuff():
                 for y in range(height):
                     for x in range(width):
                         pixels[x,y] = (pixels[x,y][channel], pixels[x,y][channel], pixels[x,y][channel])
-                save_image(img, str(channel) + "_" + file)
+                if(invert):
+                    for y in range(height):
+                        for x in range(width):
+                            pixels[x,y] = (255 - pixels[x,y][0], 255 - pixels[x,y][1], 255 - pixels[x,y][2])
+                save_image(img, str(invert) + "-inverted_" + str(channel) + "_" + file)
     
 if __name__ == "__main__":
     do_stuff()
